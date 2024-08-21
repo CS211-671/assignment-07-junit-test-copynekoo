2622c838e0ef6230662826373871205a972f63f0
copynekoo <srisil.s@ku.th>
2024-08-21 14:57:57 +0700

    Add testGiveScoreToId

--- src/test/java/ku/cs/models/StudentListTest.java
+++ src/test/java/ku/cs/models/StudentListTest.java
@@ -18,4 +18,17 @@ class StudentListTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void testGiveScoreToId() {
+        StudentList studentList = new StudentList();
+        studentList.addNewStudent("001", "jason", 40.5);
+        studentList.addNewStudent("002", "freddy", 70);
+        studentList.addNewStudent("003", "mickey", 93);
+
+        studentList.findStudentById("002").addScore(10);
+        double expected = 80;
+        double actual = studentList.findStudentById("002").getScore();
+        assertEquals(expected, actual);
+    }
 }
